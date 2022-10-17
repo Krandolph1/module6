a310a762a840243dfa2b95f8a4fc20125fb51a1f
Krandolph1 <kensee1116@gmail.com>
2022-10-17 13:20:42 -0400

    coI change the slideshow in main.js,all the pages i put in script src, main.css i change the slideshow

--- script/main.js
+++ script/main.js
@@ -1,28 +1,28 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+document.getElementById("userName").innerHTML = "Bearded Lizard";
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+document.getElementById("userName").style.color = "red";
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
 
-function showSlides(n) {
+
+
+
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
   let i;
   let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("demo");
-  let captionText = document.getElementById("caption");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  let dots = document.getElementsByClassName("dot");
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+    slides[i].style.display = "none";  
   }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
+  slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
